0ccbc05a6c32a8c68b42ceefbfaf7ab316b9a3b3
Jason Reid <jasonreid@netconstruct.co.uk>
2022-03-24 21:58:25 +0000

    Improved terminal output readability

--- antFacts.js
+++ antFacts.js
@@ -13,14 +13,16 @@ const weekday = [
   "Saturday",
 ];
 
-console.log(`Happy ${weekday[date.getDay()]}!`);
+// log = misc. info = question. warn = answer.
+console.log(`------------------------`);
+console.info(`Happy ${weekday[date.getDay()]}!`);
 
 await onThisDay(date);
 
 await wordOfTheDay(date);
 
 // Facts
-fetch("https://www.reddit.com/r/todayilearned/new.json")
+fetch(`https://www.reddit.com/r/todayilearned/new.json`)
   .then((response) => response.json())
   .then(async (realData) => {
     const realFacts = realData.data.children;
@@ -32,7 +34,7 @@ fetch("https://www.reddit.com/r/todayilearned/new.json")
     let fakeFact = process.env.FAKE_FACT_OVERRIDE;
     if (!fakeFact) {
       const response = await fetch(
-        "https://www.reddit.com/r/FakeFacts/new.json"
+        `https://www.reddit.com/r/FakeFacts/new.json`
       );
       const fakeData = await response.json();
       let fakeFactApiSkip = process.env.FAKE_FACT_TAKE;
@@ -46,15 +48,17 @@ fetch("https://www.reddit.com/r/todayilearned/new.json")
       [facts[i], facts[j]] = [facts[j], facts[i]];
     }
 
-    console.log("Todays facts are...");
+    console.info(`Todays facts are...`);
     facts.forEach((fact, index) => {
-      console.log(`${index + 1}. ${fact}`);
+      console.info(`${index + 1}. ${fact}`);
     });
-    console.log("Spot the false fact!");
+    console.info(`Spot the false fact!`);
 
-    console.warn(
-      `The fake fact is: ${facts.findIndex((rank) => rank === fakeFact) + 1}`
-    );
+    console.log(`------------------------`);
+    console.warn(`Todays false fact is....`);
+    console.warn(`${fakeFact}`);
+    console.warn(`Well done to, these people:`);
+    console.log(`------------------------`);
   });
 
 function trimTIL(fact) {
@@ -79,7 +83,7 @@ async function wordOfTheDay() {
   ).text();
   const meaning = $(".wod-definition-container > p:nth-child(2)").text();
 
-  console.log(`Todays word of the day is ${word}! ${meaning}`);
+  console.info(`Todays word of the day is ${word}! ${meaning}`);
 }
 
 async function onThisDay() {
@@ -87,7 +91,7 @@ async function onThisDay() {
     `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${date.getMonth()}/${date.getDate()}`
   );
   const onThisDayData = await response.json();
-  console.log(
+  console.info(
     `On this day in History in ${onThisDayData.selected[0].year}, ${onThisDayData.selected[0].text}`
   );
 }
